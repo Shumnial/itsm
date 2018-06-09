2573e100c9a251a20e882dff4eddcc7c74b53ce6
Nikita Shumakov <shumakov.n@yandex.ru>
2018-06-09 17:03:48 +0500

    up

--- public/javascripts/sources/common.js
+++ public/javascripts/sources/common.js
@@ -23,23 +23,33 @@ $(() =>{
 	}
 
 	function calcWorkers() {
-		return Math.floor(+calcInputs["average-salary"].val() * 1.42);
+		let val = calcInputs["average-salary"].val();
+		return Math.floor(+val * 1.42);
 	}
 
 	function calcAvgSalary() {
-		return Math.floor(calcWorkers() * +calcInputs["workers"].val());
+		let val = calcInputs["workers"].val();
+		console.log("val: ", val);
+		return Math.floor(calcWorkers() * +val);
 	}
 
 	function calcRatio() {
-		return Math.floor(calcAvgSalary() / +calcInputs["ratio"].val() * 100);
+		let val = calcInputs["ratio"].val();
+		
+		console.log("val: ", val);
+		return Math.floor(calcAvgSalary() / +val * 100);
 	}
 
 	function calcMargin() {
-		return Math.floor(calcRatio() / +calcInputs["margin"].val() * 100);
+		let val = calcInputs["margin"].val();
+		
+		console.log("val: ", val);
+		return Math.floor(calcRatio() / +val * 100);
 	}
 
 	function calcWorkdays() {
-		return Math.floor(calcMargin() / +calcInputs["workdays"].val() * 100);
+		let val = calcInputs["workdays"].val();
+		return Math.floor(calcMargin() / +val);
 	}
 
 	function getResult(valueName, funcName) {
@@ -59,25 +69,6 @@ $(() =>{
 		$('.calc__btn').text('Пересчитать');
 	}
 
-	const links = $('.main-nav__link');
-
-	let linksWidth = {
-		"0": "",
-		"1": "",
-		"2": "",
-		"3": "",
-		"4": ""
-	}
-
-	for (let i = 0; i < links.length; i++) {
-		linksWidth[i] = links.eq(i).outerWidth();
-	};
-
-	let previousId = 0;
-	let previousWidth = 0;
-
-	
-
 	const navCoords = $('.intro').offset().top;
 	function onScroll () {
 		const target = $('.main-nav');
@@ -90,97 +81,107 @@ $(() =>{
 		}
 	};
 
-	 
+	const sections = $('.section[id]');
+	const progressEl = $('.main-nav__progress');
+	const row = $('.main-nav__row');
+	const positions = {};
+	const links = $('.main-nav__link');
+	const lastTarget = $('.main-nav__link').last().attr('href').slice(1);
+	let spyScroll = true;
+	let progressWidth = 0;
+	let progressMinWidth = 0;
+
+	function onLinkClick(evt) {
+		evt.preventDefault();
+		const target = $(this);
+		const id  = $(this).attr('href');
+		const top = $(id).offset().top + 10;
+		spyScroll = false;
+
+		$('body, html').animate({scrollTop: top}, 1000, () => {
+			spyScroll = true;
+
+			scrollHandler.call(null)
+		});
+	}
+
+	function displayMoveUpLink () {
+		const target = $('.move-up');
+		let halfHeight = $('html').height() / 3;
+		let currentPos = $(window).scrollTop();
+		if (currentPos >= halfHeight) {
+			target.removeClass('move-up--hidden');
+		} else {
+			target.addClass('move-up--hidden');
+		}
+	}
+	
+	function getPositions () {
+		progressMinWidth = row.offset().left;
+		sections.each((idx, el) => {
+			positions[el.id]= $(el).offset().top
+		});
+	}
+
+	function getComputedWidth (target) {
+
+		if(!target) {
+			links.removeClass('is-active');
+			return progressMinWidth
+		}
+
+		const el = $(`.main-nav__link[href="#${target}"]`);
+
+		if(target === lastTarget) {
+			setActive(el);
+			$('.move-up').addClass('is-active');
+			return '100%';
+		} else {
+			$('.move-up').removeClass('is-active');
+		}
+
+		const w = el.outerWidth() || 0;
+		const p = el.offset().left || 0;
+		const result = p + w;
+
+		setActive(el);
+		return result <= 0 ? progressMinWidth : result
+	}
+
+	function setActive (el) {
+		el.addClass('is-active');
+		$(`.main-nav__link:lt(${el.index()})`).addClass('is-active');
+		$(`.main-nav__link:gt(${el.index()})`).removeClass('is-active');
+	}
+
+	function scrollHandler () {
+		if(!spyScroll) return;
+		spyScroll = true
+		const top = $(window).scrollTop();
+		let target = null;
+		for (let i in positions) {
+			if (positions[i] <= top) {
+				target = i;
+			}
+		}
+		const w = getComputedWidth(target);
+		progressEl.width(w);
+	}
+
+	displayMoveUpLink();
+	getPositions();
+	scrollHandler();
 
 	$('.calc__form').on('submit', onCalcSubmit);
 	onScroll();
-	$(document)
-		.on('scroll', onScroll);
 
-	!function() {
-		const sections = $('.section[id]');
-		const progressEl = $('.main-nav__progress');
-		const row = $('.main-nav__row');
-		const positions = {};
-		const links = $('.main-nav__link');
-		const lastTarget = $('.main-nav__link').last().attr('href').slice(1);
-		let spyScroll = true;
-		let progressWidth = 0;
-		let progressMinWidth = 0;
+	$(document).on('scroll', onScroll);
 
+	links.on('click', onLinkClick);
+	$('.move-up').on('click', onLinkClick);
 
-		links.on('click', onLinkClick)
-
-		function onLinkClick(evt) {
-			evt.preventDefault();
-			const target = $(this);
-			const id  = $(this).attr('href');
-			const top = $(id).offset().top + 10;
-			spyScroll = false;
-
-			$('body, html').animate({scrollTop: top}, 1000, () => {
-				spyScroll = true;
-
-				scrollHandler.call(null)
-			});
-
-			
-		}
-		
-		function getPositions () {
-			progressMinWidth = row.offset().left;
-			sections.each((idx, el) => {
-				positions[el.id]= $(el).offset().top
-			});
-		}
-
-		function getComputedWidth (target) {
-
-			if(!target) {
-				links.removeClass('is-active');
-				return progressMinWidth
-			}
-
-			const el = $(`.main-nav__link[href="#${target}"]`);
-
-			if(target === lastTarget) {
-				setActive(el);
-				return '100%';
-			}
-
-			const w = el.outerWidth() || 0;
-			const p = el.offset().left || 0;
-			const result = p + w;
-
-			setActive(el);
-			return result <= 0 ? progressMinWidth : result
-		}
-
-		function setActive (el) {
-			el.addClass('is-active');
-			$(`.main-nav__link:lt(${el.index()})`).addClass('is-active');
-			$(`.main-nav__link:gt(${el.index()})`).removeClass('is-active');
-		}
-
-		function scrollHandler () {
-			if(!spyScroll) return;
-			spyScroll = true
-			const top = $(window).scrollTop();
-			let target = null;
-			for (let i in positions) {
-				if (positions[i] <= top) {
-					target = i;
-				}
-			}
-			const w = getComputedWidth(target);
-			progressEl.width(w)
-		}
-
-		getPositions();
-		scrollHandler();
-
-		$(window)
-			.on('resize', $.debounce(100, getPositions))
-			.on('scroll', $.debounce(100, scrollHandler))
-	}();
+	$(window)
+	.on('resize', $.debounce(100, getPositions))
+	.on('scroll', $.debounce(100, scrollHandler))
+	.on('scroll', $.debounce(100, displayMoveUpLink));
 })
